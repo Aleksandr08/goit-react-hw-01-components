afa34a32597bc89990da9e6bcde11acd7601bff5
Your Name <sandrol080888@gmail.com>
2020-09-10 01:08:46 +0300

    commit

---
+++ build/precache-manifest.d16a2620ac5d7f921d21a019df520183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fe0efae9bdecdecd1b539da30aead6",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "dcb4c3b0600276c51ef3",
+    "url": "/goit-react-hw-01-components/static/css/main.4b0e707a.chunk.css"
+  },
+  {
+    "revision": "8d9dc58c1a4c85358da3",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcb4c3b0600276c51ef3",
+    "url": "/goit-react-hw-01-components/static/js/main.fd28849e.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
